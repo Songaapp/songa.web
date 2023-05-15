ce4289383c04cd7fd6e0eb0b46c440210d1daf67
Peter Michuki <133080797+Pmichuki@users.noreply.github.com>
2023-05-15 07:28:17 +0300

    Update index.tsx

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,30 +1,17 @@
-//div element for the about section
-const aboutSection = document.createElement('div');
-aboutSection.classList.add('about-section');
+import React from 'react';
 
-// heading element for the section title
-const aboutTitle = document.createElement('h2');
-aboutTitle.innerText = 'About Us';
-aboutTitle.classList.add('about-title');
+const AboutSection = () => {
+  return (
+    <div className="about-section">
+      <h2 className="about-title">About Us</h2>
+      <p className="about-content">The BODABODA sector is one of the most important contributions to the Kenyan economy. On average,
+        8 Kenyans rely on one ride for their daily activities, totaling 22 million daily rides in Kenya. With 2 million registered
+        boda riders in Kenya, this business produces ksh.370 million each day. Despite this strong figure this business is less 
+        controlled by the government, making it a renegade industry. At SONGA, we are attempting to dispel this misconception 
+        by providing secure, convenient, and economical trips that leave the consumer with the impression that THIS IS MADE FOR ME.</p>
+      <button className="learn-more-button" onClick={() => window.location.href = 'https://example.com'}>Learn More</button>
+    </div>
+  );
+};
 
-//  paragraph element for the section content
-const aboutContent = document.createElement('p');
-aboutContent.innerText = 'The BODABODA sector is one of the most important contributions to the Kenyan economy. On average, 8 
-Kenyans rely on one ride for their daily activities, totaling 22 million daily rides in Kenya. With 2 million registered boda 
-riders in Kenya, this business produces ksh.370 million each day. Despite this strong figure this business is less controlled by
-the government, making it a renegade industry. At SONGA, we are attempting to dispel this misconception by providing secure, 
-convenient, and economical trips that leave the consumer with the impression that THIS IS MADE FOR ME.';
-aboutContent.classList.add('about-content');
-
-aboutSection.appendChild(aboutTitle);
-aboutSection.appendChild(aboutContent);
-
-const learnMoreButton = document.createElement('button');
-learnMoreButton.innerText = 'Learn More';
-learnMoreButton.classList.add('learn-more-button');
-const url = 'https://example.com';
-learnMoreButton.addEventListener('click', () => {
-  window.location.href = url;
-});
-aboutSection.appendChild(learnMoreButton);
-document.body.appendChild(aboutSection);
+export default AboutSection;
